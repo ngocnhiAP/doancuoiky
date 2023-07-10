061813d3b52611b644da8a2bb8dcac7e34c2d854
ngocnhiAP <ngocnhi170103@gmail.com>
2023-07-10 16:02:49 +0700

    up code lan 6

--- wp-login.php
+++ wp-login.php
@@ -38,7 +38,7 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Tự động đăng nhập</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Đăng nhập">
-									<input type="hidden" name="redirect_to" value="http://localhost:8080/Doancuoiky/wp-admin/?simply_static_page=4189">
+									<input type="hidden" name="redirect_to" value="http://localhost:8080/Doancuoiky/wp-admin/?simply_static_page=5392">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
@@ -63,7 +63,7 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
 <option value="vi" lang="vi" selected data-installed="1">Tiếng Việt</option></select>
 					
-											<input type="hidden" name="redirect_to" value="http://localhost:8080/Doancuoiky/wp-admin/?simply_static_page=4189">
+											<input type="hidden" name="redirect_to" value="http://localhost:8080/Doancuoiky/wp-admin/?simply_static_page=5392">
 					
 					
 						<input type="submit" class="button" value="Thay đổi">
